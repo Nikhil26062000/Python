f1bd2781ab16a2285bd7c6f66b024735100d4dfc
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-07-07 15:22:35 +0530

    List-&-Tuples/List-Tuples.py

---
+++ List-&-Tuples/List-Tuples.py
@@ -0,0 +1,24 @@
+
+# List
+marks = [2,3,4,5,67,43.21,"karan"]
+print(marks)
+print(type(marks))
+marks[0] = "Ram"
+print(marks)
+print(marks[0:4])
+
+#slicing in List (neg index)
+print(marks[-4:-1])
+
+
+#? Tuple
+tup = (1,3,5,4,2)
+print(type(tup))
+
+# empty tuple
+tup2 = ()
+
+# tuple with single vlaue
+tup3 = (1,) 
+#* remember if we remove "," from paranthesis then python treat this single vlaue as integer and not tuple
+
